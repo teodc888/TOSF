330c7257fa293569c8b73aa284b746c8b81a980d
mateo <teodc4447@gmail.com>
2022-02-01 14:31:36 -0300

    logeo auth0

---
+++ src/components/perfil/perfil.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function Perfil() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : isAuthenticated ? (
+        <div>
+          <h1>{user.name}</h1>
+          <img src={user.picture} alt="Profile" />
+          <p>{user.email}</p>
+        </div>
+      ) : (
+        <div>Not logged in</div>
+      )}
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-1nqkyjy8.us.auth0.com"
+      clientId="mmk7UOexLEFB6EhjcxsQOq4UPVK9vZha"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
